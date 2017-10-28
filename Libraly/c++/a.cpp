d5b069f6666a49e611298ad765b57093a9a5938a
コンブ <konbu1610@gmail.com>
2017-10-29 05:02:01 +0900

    template

---
+++ Libraly/c++/a.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+  
+using namespace std;
+  
+#define rep(i,n) REP(i,0,n)
+#define REP(i,s,e) for(int i=(s); i<(int)(e); i++)
+#define repr(i, n) REPR(i, n, 0)
+#define REPR(i, s, e) for(int i=(int)(s-1); i>=(int)(e); i--)
+#define pb push_back
+#define all(r) r.begin(),r.end()
+#define rall(r) r.rbegin(),r.rend()
+#define fi first
+#define se second
+  
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+ 
+const int INF = 1e9;
+const ll MOD = 1e9 + 7;
+double EPS = 1e-8;
+
+
+int main(){
+    
+}
