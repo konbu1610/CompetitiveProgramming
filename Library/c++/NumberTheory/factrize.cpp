18de994e7c9aa069cb8524a837e5f5c8a1c34fb7
コンブ <konbu1610@gmail.com>
2018-01-13 00:19:31 +0900

    add factrization

---
+++ Library/c++/NumberTheory/factrize.cpp
@@ -0,0 +1,20 @@
+/*
+  factrization
+  O( sqrt(n) )
+  verified yukicoder No. 3
+ */
+
+using Facts = map<ll, int>; 
+void factrize(ll n, Facts& mp) {
+    if(n < 2) return;
+    ll m = sqrt(n);
+    REP(i, 2, m+1) {
+        int cnt = 0;
+        while(n%i == 0) {
+            n /= i;
+            ++cnt;
+        }
+        mp[i] += cnt;
+    }
+    if(n != 1) mp[n]++;
+}
