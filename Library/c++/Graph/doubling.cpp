59d8de274be476ea61fbb82f3cb716d1d5de99b5
konbu <yutakase.1610@gmail.com>
2019-12-11 10:46:42 +0900

    add doubling

---
+++ Library/c++/Graph/doubling.cpp
@@ -0,0 +1,47 @@
+// ダブリング
+// cost 0 での遷移を書いてからpreCalcする！
+// verify 
+//  getCost ARC060E
+//  getNode ABC136D
+namespace Doubling {
+    using NODE_TYPE = int;
+    using COST_TYPE = long long;
+    // d[i][0]はあらかじめ計算しておく！, iはトポロジカルソート後のidx
+    void preCalc(vector<vector<NODE_TYPE>>& d) {
+        int n = d.size();
+        int m = d[0].size();
+        REP(j, 1, m) rep(i, n) {
+            d[i][j] = d[d[i][j-1]][j-1];
+        }
+    }
+    // 頂点uからcostでいける頂点を求める
+    NODE_TYPE getNode(NODE_TYPE u, COST_TYPE cost, const vector<vector<NODE_TYPE>>& d) {
+        int n = d.size();
+        int m = d[0].size();
+        NODE_TYPE cur = u;
+        repr(j, m) {
+            if(cost&(COST_TYPE(1)<<j)) {
+                cur = d[cur][j];
+            }
+        }
+        return cur;
+    }
+    // 頂点uから頂点vまでのコストを求める
+    COST_TYPE getCost(NODE_TYPE u, NODE_TYPE v, const vector<vector<NODE_TYPE>>& d) {
+        if(u >= v) assert(false);
+        int n = d.size();
+        int m = d[0].size();
+        int cur = u;
+        COST_TYPE ret = 0;
+        while(cur < v) {
+            if(d[cur][0] >= v) {
+                ret += 1;
+                break;
+            }
+            int j = lower_bound(all(d[cur]), v) - d[cur].begin() - 1;
+            cur = d[cur][j];
+            ret+= COST_TYPE(1) << j;
+        }
+        return ret;
+    }
+};
